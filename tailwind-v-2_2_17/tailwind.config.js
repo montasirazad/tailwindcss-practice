d5d7fa7f89cf1d695794ac2c2cb2b2d845144501
Montasir Azad <montasirazad@gmail.com>
2026-07-04 02:58:34 +0600

    practice done

---
+++ tailwind-v-2_2_17/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html", "./src/**/*.{html,js}"],
+  purge: [],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
